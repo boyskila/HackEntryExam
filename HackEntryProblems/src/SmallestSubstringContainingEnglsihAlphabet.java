1921517435dcd705b63a683aa24c8099bfaab8b8
boyskila <bmlalov@abv.bg>
2015-05-11 00:17:47 +0300

    change file

--- HackEntryProblems/src/SmallestSubstringContainingEnglsihAlphabet.java
+++ HackEntryProblems/src/SmallestSubstringContainingEnglsihAlphabet.java
@@ -1,71 +1,39 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
- 
+
 public class SmallestSubstringContainingEnglsihAlphabet {
- 
-    public static boolean validateString(String str) {
- 
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isWhitespace(str.charAt(i)) && !str.matches("^\\p{ASCII}*$")) {
-                return false;
-            }
-        }
-        return true;
+    private static List<Character> loadAlphabet() {
+        List<Character> alphabetList = new ArrayList<>(26);
+        for (char c = 'a'; c <= 'z'; c++)
+            alphabetList.add(c);
+        return alphabetList;
     }
- 
-    public String findSubstring(String str) {
- 
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        List<Character> alphabetList = new ArrayList<Character>();
- 
-        char[] inputedString = str.toLowerCase().toCharArray();
-        int substringSize = inputedString.length + 1;
+
+    private static String findSubstring(String str) {
+        boolean isValid = str.contains(" ") ? false : !str.matches("^\\p{ASCII}*$") ? false : true;
         String result = null;
- 
-        if (validateString(str)) {
- 
-            for (char letter : alphabet.toCharArray()) {
-                alphabetList.add(letter);
-            }
- 
-            for (int i = 0; i < inputedString.length; i++) {
- 
-                List<Character> subString = new ArrayList<Character>();
- 
-                for (int j = i; j < inputedString.length; j++) {
- 
-                    subString.add(inputedString[j]);
- 
+        if (isValid) {
+            List<Character> alphabetList = loadAlphabet();
+            for (int i = 0; i < str.length(); i++) {
+                List<Character> subString = new ArrayList<>();
+                for (int j = i; j < str.length(); j++) {
+                    subString.add(str.charAt(j));
                     if (subString.containsAll(alphabetList)) {
- 
-                        if (substringSize > subString.size()) {
-                            substringSize = subString.size();
+                        if (result == null || result.length() > subString.size())
                             result = str.substring(i, j + 1);
-                            break;
-                        }
+                        break;
                     }
                 }
             }
- 
-        } else {
+        } else
             return "Invalid String";
-        }
         return result;
     }
- 
+
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        String str = "abcdefghijklmn124345678!@#$%^&*opqrstuvwxyz!*abcdefghijklmn";
-        String str1 = "aaaaaabcdefghijklmnopqrstuvwxyz";
-        // String str =
-        // "aa1bcdefghijklmnopqrstuvwxyza2bcdefghijklmnopqrstuvwxyz";
-        // String str =
-        // "abcde*&^%$#fkSlmnghij1234opqrtuvwxyz...abcdefghi11111111111111111111111111jklmnopqrstuv111wxyz";
-        SmallestSubstringContainingEnglsihAlphabet sm = new SmallestSubstringContainingEnglsihAlphabet();
-        System.out.println(sm.findSubstring(str));
-        System.out.println(sm.findSubstring(str1));
-        System.out.println("The program was execfuted for: " + (System.currentTimeMillis() - start) + " milliseconds.");
- 
+        System.out.println(findSubstring("abcdefghijklmn124345678!@#$%^&*opqrstuvwxyz!*abcdefghijklmn"));
+        System.out.println(findSubstring("aaaaaabcdefghijklmnopqrstuvwxyz"));
+        System.out.println(findSubstring("aa1bcdefghijklmnopqrstuvwxyza2bcdefghijklmnopqrstuvwxyz"));
+        System.out.println(findSubstring("abcde*&^%$#fkSlmnghij1234opqrtuvwxyz...abcdefghi1111111jklmnopqrstuv1wxyz"));
     }
 }
